49818fcdc964e84c3480867bd1d33e0355a73657
Lane le Prevost-Smith <lanelps@gmail.com>
2019-02-11 13:56:10 +1300

    added create-react-app

---
+++ build/precache-manifest.a2f4dd55ff9dee488ab8ef41542e9cd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "217bea19450b5c3b69b2",
+    "url": "/static/css/main.074bd13e.chunk.css"
+  },
+  {
+    "revision": "217bea19450b5c3b69b2",
+    "url": "/static/js/main.217bea19.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af37e26e4877e56580bf383485c999ac",
+    "url": "/index.html"
+  }
+];
